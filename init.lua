8c32d9755ce59a45945c172d6a121e0a5d526dac
jamcshan <jackrmcshane@gmail.com>
2024-09-29 11:15:44 -0400

    added user defined commands file

--- init.lua
+++ init.lua
@@ -44,6 +44,7 @@ vim.g.maplocalleader = ' '
 require 'settings'
 require 'keymaps'
 require 'autocmds'
+require 'commands'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
